3ed8596b3677d81fa5d23c4ab5e8bc0a11cbb88e
prasant-shukla <shukla.prasant.7@gmail.com>
2025-03-21 15:52:10 +0530

    Create streamlit_app.py
    Copied from my custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,44 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+helpful_links = [
+    "https://docs.streamlit.io",
+    "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
+    "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
+    "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
+]
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write("Choose the fruits you want in your custom Smoothie!")
+
+name_on_order = st.text_input('Name on Smoothie')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')).to_pandas()
+
+# Smoothie ingredient selection with a limit of 5 items
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe['FRUIT_NAME'].tolist(),
+    max_selections=5
+)
+
+if ingredients_list:
+    ingredients_string = ', '.join(ingredients_list)
+
+    # SQL insertion statement
+    my_insert_stmt = f"""INSERT INTO smoothies.public.orders (ingredients, NAME_ON_ORDER)
+        VALUES ('{ingredients_string}', '{name_on_order}')"""
+
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        try:
+            session.sql(my_insert_stmt).collect()
+            st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+        except Exception as e:
+            st.error(f'Error while placing order: {e}')
